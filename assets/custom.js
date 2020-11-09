aac1f24fd9d50e10a08af8ed46c6b8e4a12d732f
Pooja Thakor <pooja.thakor99@gmail.com>
2020-11-09 18:45:35 +0530

    Feature: slider changes

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,7 @@
+// vanilla JS
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Product switcher in Collection switcher section
     var carousels = document.querySelectorAll('[data-products-slider]');
     carousels.forEach(function(carousel, index) {
         var flkty = new Flickity(carousel, {
@@ -9,13 +12,27 @@ document.addEventListener('DOMContentLoaded', function() {
             wrapAround: false
         });
     });
+    // Collection switcher
     document.addEventListener('click', function(event) {
         if (!event.target.matches('[data-collection-switch]')) return;
         event.preventDefault();
-        let collectionLoopId = (event.target).getAttribute('data-collection-loop-id');
-        var productSlides = document.querySelectorAll('[data-products-slider]');
+        let collectionLoopId = parseInt((event.target).getAttribute('data-collection-loop-id'));
+        let selectedCollections = document.querySelectorAll('[data-collection-switch]');
+        selectedCollections.forEach(function(selectedCollection, index) {
+            if (index + 1 === collectionLoopId) {
+                selectedCollection.classList.add("active");
+            } else {
+                selectedCollection.classList.remove("active");
+            };
+        });
+
+        let productSlides = document.querySelectorAll('[data-products-slider]');
         productSlides.forEach(function(productSlide, index) {
-            console.log(productSlides);
+            if (index + 1 == collectionLoopId) {
+                productSlide.classList.add("active");
+            } else {
+                productSlide.classList.remove("active");
+            }
         });
     }, false);
 });
